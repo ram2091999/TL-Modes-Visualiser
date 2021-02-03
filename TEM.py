2dc8ed592aaea2dd7428245f33fd105de0d57e30
Sai Mitheran <49593646+smj007@users.noreply.github.com>
2021-02-03 22:39:19 +0530

    Final commit - update1

---
+++ TEM.py
@@ -0,0 +1,41 @@
+import math
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy import special
+from functions_rect import TEM_Functions
+import pandas as pd
+
+PI = math.pi
+A = 4
+B = 8
+
+
+def handleTEM(st):
+    r = np.linspace(A,B,101)
+    t = np.linspace(0,2*PI,101)
+    T,RAD = np.meshgrid(t,r)
+    U = 10/RAD
+    V = T*0
+    par = TEM_Functions()
+    col1, col2 = st.beta_columns(2)
+    col1.header("E Field")
+    col2.header("H Field")
+    fig,ax = plt.subplots()
+    plt.polar(2*PI,B)
+    plt.streamplot(T,RAD,V,U, color="xkcd:azure")
+    plt.axis("scaled")
+    col1.pyplot(fig)
+    U = 0.000000001*RAD
+    V=T*100
+    fig,ax = plt.subplots()
+    plt.polar(2*PI,B)
+    plt.streamplot(T,RAD,V,U, color="red")
+    plt.axis("scaled")
+    col2.pyplot(fig)
+    st.write(pd.DataFrame({
+           'Parameter': ["Kc", "Fc", "Beta-g", "Vg"],
+           'Value': [par.Kc(),par.Fc(), par.beta_g(),par.v_G()],
+           'Unit':["1/m", "Hz", "1/m", "m/s"]
+
+       }))
+
